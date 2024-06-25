e071238da785a9afce63be50e4e6c442a9b0ff05
SaungThetNgonOo <saung1872000@gmail.com>
2024-06-25 15:09:31 +0630

    addMyConnection

--- boundaryOfLaws/src/main/java/boundaryOfLaws/persistance/MyConnection.java
+++ boundaryOfLaws/src/main/java/boundaryOfLaws/persistance/MyConnection.java
@@ -1,5 +1,25 @@
 package boundaryOfLaws.persistance;
 
-public class MyConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MyConnection {
+	static Connection con=null;
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String db="jdbc:mysql://localhost:3306/Lawdb";
+			String username="root";
+			String password="root";
+			con=DriverManager.getConnection(db,username,password);
+			System.out.println("Database Connecting.....");
+		}catch(ClassNotFoundException e) {
+			System.out.println("Driver class not found");
+		}catch(SQLException e) {
+			System.out.println("Database connection is not OK"+e);
+		}
+		return con;
+	}
 }
